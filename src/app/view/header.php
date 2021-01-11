f1b7e9d7f03191351ca224f0195c4e0a8081ae4f
qipeng <qp19940413@163.com>
2021-01-11 11:54:08 +0900

    Nav style.

--- src/app/view/header.php
+++ src/app/view/header.php
@@ -2,12 +2,34 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no,
+     initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" href="../../static/img/site.png">
+    <link rel="stylesheet" href="../../static/css/uikit.min.css" type="text/css"/>
     <link rel="stylesheet" href="../../static/css/main.css" type="text/css"/>
-    <title><?php echo $title ?></title>
+    <link rel="icon" href="../../static/img/site.png">
+    <script type="text/javascript" src="../../static/js/uikit.min.js"></script>
+    <script type="text/javascript" src="../../static/js/uikit-icons.min.js"></script>
+    <script type="text/javascript" src="../../static/js/jquery-3.5.1.min.js"></script>
+    <title>HOME</title>
 </head>
 <body>
-<h1><?php echo $title ?></h1>
+
+<!-- navigation -->
+<div>
+    <nav class="uk-navbar-container" uk-navbar>
+        <div class="uk-navbar-left">
+            <ul class="uk-navbar-nav">
+                <li class="uk-active"><a href="#">HOME | ホーム</a></li>
+                <li><a href="#">BLOG | ブログ</a></li>
+                <li><a href="#">OTHER | その他</a></li>
+            </ul>
+        </div>
+
+        <div class="uk-navbar-right">
+            <ul class="uk-navbar-nav">
+                <li class="uk-active"><a href="#">LOGIN | 登録</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
